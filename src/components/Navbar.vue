8f76d7325992a97a3b46e5750dedb98a4d44c843
Yamin Alam Sarker <yaminalam27@gmail.com>
2018-02-17 00:01:48 +0600

    search by OnKeyUp

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -16,7 +16,7 @@
                 </li>
             </ul>
             <form @submit.prevent="search" class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" v-model="keyword" placeholder="Search">
+                <input class="form-control mr-sm-2" type="search" v-model="keyword" placeholder="Search" @keyup="search">
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
         </div>
